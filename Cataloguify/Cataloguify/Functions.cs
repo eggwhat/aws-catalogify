ec2f3437759a5aef2745ee3eaf7d8a6670835a64
eggwhat <piotr.padamczyk@gmail.com>
2024-06-01 18:40:44 +0200

    add function for generating tokens

--- Cataloguify/Cataloguify/Functions.cs
+++ Cataloguify/Cataloguify/Functions.cs
@@ -3,6 +3,14 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.APIGateway;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using AuthLambda;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -41,5 +49,33 @@ public class Functions
         context.Logger.LogInformation("Handling the 'Get' Request");
 
         return HttpResults.Ok("Hello AWS Serverless");
+    }
+
+    private const string key = "S0M3RAN0MS3CR3T!1!MAG1C!1!";
+
+    [LambdaFunction]
+    [RestApi(LambdaHttpMethod.Post, "/generate-token")]
+    public async Task<string> GenerateTokenAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        var tokenRequest = JsonConvert.DeserializeObject<User>(request.Body);
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dbContext = new DynamoDBContext(client);
+
+        //check if user exists in ddb
+        var user = await dbContext.LoadAsync<User>(tokenRequest?.Email);
+        if (user == null) throw new Exception("User Not Found!");
+        if (user.Password != tokenRequest.Password) throw new Exception("Invalid Credentials!");
+        var token = GenerateJWT(user);
+        return token;
+    }
+
+    public string GenerateJWT(User user)
+    {
+        var claims = new List<Claim> { new(ClaimTypes.Email, user.Email), new(ClaimTypes.Name, user.Username) };
+        byte[] secret = Encoding.UTF8.GetBytes(key);
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(claims: claims, expires: DateTime.UtcNow.AddMinutes(15), signingCredentials: signingCredentials);
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
     }
 }
