130a15e68b5ae00ac34b53a6e7a84e41626f6a20
Amadeusz Nowak <an2508374@gmail.com>
2024-06-04 17:34:17 +0200

    (#2) add layout of sign in page

--- Cataloguify/Cataloguify.Client/Models/SignInModel.cs
+++ Cataloguify/Cataloguify.Client/Models/SignInModel.cs
@@ -1,9 +1,7 @@
 namespace Cataloguify.Client.Models;
 
-public class SignUpModel
+public class SignInModel
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
 }
