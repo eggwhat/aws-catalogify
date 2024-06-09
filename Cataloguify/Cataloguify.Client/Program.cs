31d0f6fad35227d2df7890be7878bf0532614a39
Amadeusz Nowak <an2508374@gmail.com>
2024-06-10 01:15:43 +0200

    (#2) update Program.cs file

--- Cataloguify/Cataloguify.Client/Program.cs
+++ Cataloguify/Cataloguify.Client/Program.cs
@@ -4,21 +4,18 @@ using Blazored.LocalStorage;
 using Cataloguify.Client;
 using Cataloguify.Client.Areas.Identity;
 using Cataloguify.Client.HttpClients;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var httpClientOptions = builder.Configuration.GetSection("HttpClientOptions").Get<HttpClientOptions>();
-
-// builder.Services.AddSingleton(httpClientOptions);
-
 builder.Services.AddHttpClient<IHttpClient, CustomHttpClient>((serviceProvider, client) =>
 {
-    var options = serviceProvider.GetRequiredService<HttpClientOptions>();
-    client.BaseAddress = new Uri(options.ApiUrl); 
+    var options = builder.Configuration.GetSection("HttpClientOptions").Get<HttpClientOptions>();
+    if (options != null)
+    {
+        client.BaseAddress = new Uri(options.ApiUrl);   
+    }
 });
 
 builder.Services.AddBlazoredLocalStorage(); 
