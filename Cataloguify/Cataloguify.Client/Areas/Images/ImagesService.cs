04f6c81c0a64ec9f5ad6594a137e82980c94c4c0
Amadeusz Nowak <an2508374@gmail.com>
2024-06-14 21:02:33 +0200

    (#2) fix handling of http response for search images

--- Cataloguify/Cataloguify.Client/Areas/Images/ImagesService.cs
+++ Cataloguify/Cataloguify.Client/Areas/Images/ImagesService.cs
@@ -21,11 +21,11 @@ public class ImagesService : IImagesService
         return await _httpClient.PostAsync<object, object>("upload-image", new { image });
     }
 
-    public async Task<HttpResponse<object>> SearchImagesAsync(IEnumerable<string> tags, int page,
+    public async Task<HttpResponse<IEnumerable<ImageDto>>> SearchImagesAsync(IEnumerable<string> tags, int page,
         int results, string sortOrder)
     {
         _httpClient.SetAccessToken(await _localStorage.GetItemAsStringAsync("Token"));
-        return await _httpClient.PostAsync<object, object>("images",
+        return await _httpClient.PostAsync<object, IEnumerable<ImageDto>>("images",
             new { tags, page, results, sortOrder });
     }
 }
