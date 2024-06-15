95a3679d66947b4772a634ffd828da625261cb97
Amadeusz Nowak <an2508374@gmail.com>
2024-06-15 20:27:01 +0200

    (#2) fix filtering images by tags

--- Cataloguify/Cataloguify.Client/Models/SearchImagesModel.cs
+++ Cataloguify/Cataloguify.Client/Models/SearchImagesModel.cs
@@ -3,6 +3,7 @@ namespace Cataloguify.Client.Models;
 public class SearchImagesModel
 {
     public IEnumerable<string> Tags { get; set; }
+    public IEnumerable<string> NotSelectedTags { get; set; }
     public int Page { get; set; }
     public int Results { get; set; }
     public string SortOrder { get; set; }
